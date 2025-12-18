5351e06f5fffc83b930667a2421b662d2fb361be
utkarshkumar21 <114558841+utkarshkumar21@users.noreply.github.com>
2025-12-18 20:44:56 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE exam_db;
+
+USE exam_db;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE exams (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(255),
+    option1 VARCHAR(100),
+    option2 VARCHAR(100),
+    option3 VARCHAR(100),
+    option4 VARCHAR(100),
+    answer INT
+);
 
